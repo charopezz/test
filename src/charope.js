76e6447a9e27806b431a87454ad204dbb7ff7822
charopezz <77083065+charopezz@users.noreply.github.com>
2021-03-12 11:37:47 -0300

    Add files via upload

---
+++ src/charope.js
@@ -0,0 +1,12 @@
+const charope = (prefix, sender) => {
+	return `Titulo
+
+teste do charope
+ainda esta. manuteçao 
+mas da para usar os
+comando 
+!sticker 
+!menuadmin`
+}
+
+exports.charope = charope
